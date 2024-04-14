37543095a5e8fbf38c77789321a4a855b149d887
Jeremi-Koloma <kolomajeremi60@gmail.com>
2024-04-14 22:24:24 +0000

    Greating Row okay

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -12,31 +12,52 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[800],
-      body: const SafeArea(
+      body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
-            padding: EdgeInsets.all(16.0),
+            padding: const EdgeInsets.all(20.0),
             child: Column(
               children: [
+                // GREATING ROW
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     // Text
-                    Text(
-                      'Hi Jeremi !',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                      ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Hi Jeremi !',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8.0),
+                        Text(
+                          '14 April, 2023',
+                          style: TextStyle(color: Colors.blue[200]),
+                        ),
+                      ],
                     ),
                     // Notification
-                    Icon(
-                      Icons.notifications,
-                      color: Colors.white,
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blue[600],
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: Icon(
+                          Icons.notifications,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ],
                 ),
+                // SEARCH BAR
               ],
             ),
           ),
