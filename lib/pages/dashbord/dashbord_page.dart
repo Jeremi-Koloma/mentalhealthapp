1bc1140a488ad8e4fd8ee6a7dc2cf37491fe9042
Jeremi-Koloma <kolomajeremi60@gmail.com>
2024-04-16 21:57:12 +0000

    header OKAY

--- lib/pages/dashbord/dashbord_page.dart
+++ lib/pages/dashbord/dashbord_page.dart
@@ -8,75 +8,87 @@ class DashbordPage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.blue[800],
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
                 children: [
-                  // TEXT
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const SizedBox(height: 25.0),
-                      const Text(
-                        'Hi Jeremi !',
-                        style: TextStyle(
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
+                      // TEXT
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 25.0),
+                          const Text(
+                            'Hi Jeremi !',
+                            style: TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(height: 8.0),
+                          Text(
+                            '20 Jan, 2024',
+                            style: TextStyle(
+                              color: Colors.blue[200],
+                            ),
+                          ),
+                        ],
                       ),
-                      const SizedBox(height: 8.0),
-                      Text(
-                        '20 Jan, 2024',
-                        style: TextStyle(
-                          color: Colors.blue[200],
+                      // NOTIFICATION
+                      Container(
+                        padding: const EdgeInsets.all(12.0),
+                        decoration: BoxDecoration(
+                          color: Colors.blue[600],
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        child: const Icon(
+                          Icons.notifications,
+                          color: Colors.white,
                         ),
                       ),
                     ],
                   ),
-                  // NOTIFICATION
+                  const SizedBox(height: 25.0),
+                  // SEARCH BAR
                   Container(
-                    padding: const EdgeInsets.all(12.0),
+                    padding: const EdgeInsets.all(16.0),
                     decoration: BoxDecoration(
                       color: Colors.blue[600],
                       borderRadius: BorderRadius.circular(12.0),
                     ),
-                    child: const Icon(
-                      Icons.notifications,
-                      color: Colors.white,
+                    child: const Row(
+                      children: [
+                        Icon(
+                          Icons.search_rounded,
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 5.0),
+                        Text(
+                          'Search',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
-              const SizedBox(height: 25.0),
-              // SEARCH BAR
-              Container(
-                padding: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.blue[600],
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-                child: const Row(
-                  children: [
-                    Icon(
-                      Icons.search_rounded,
-                      color: Colors.white,
-                    ),
-                    SizedBox(width: 5.0),
-                    Text(
-                      'Search',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
+            ),
+            const SizedBox(height: 25.0),
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
